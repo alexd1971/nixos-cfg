b645383c50d0732c6a9cdc2fb51f4c202fb7abb6
Alexey Danilevsky <alexey.danilevsky@gmail.com>
2026-08-22 09:49:17 +0300

    Document desktop applications

--- modules/common/desktop/apps.nix
+++ modules/common/desktop/apps.nix
@@ -4,20 +4,31 @@
   # Graphical applications and desktop tools available to every local user.
   environment.systemPackages = with pkgs; [
     # Everyday applications
+    # Web browser.
     firefox
+    # Graphical file manager.
     thunar
 
     # Viewers / media
+    # PDF and document viewer.
     evince
+    # Lightweight calendar popup for Waybar.
     gsimplecal
+    # Image viewer.
     imv
+    # Video and audio player.
     mpv
 
     # Desktop controls used from Sway/Waybar keybindings.
+    # Backlight brightness control.
     brightnessctl
+    # NetworkManager menu for dmenu-compatible launchers.
     networkmanager_dmenu
+    # NetworkManager tray applet and connection editor.
     networkmanagerapplet
+    # PulseAudio/PipeWire volume mixer.
     pavucontrol
+    # GTK pinentry dialog for password prompts.
     pinentry-gnome3
   ];
 }
